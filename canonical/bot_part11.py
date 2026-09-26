078d8ca83ffe5a8520b9aff62f219333bff24ae2
donkreat-eng <donkreat@yahoo.com>
2026-09-27 00:37:09 +0500

    holiday split batch 6/16

--- canonical/bot_part11.py
+++ canonical/bot_part11.py
@@ -1,13 +1,1 @@
- entry_t).total_seconds() / 3600 > MAX_HOLD_HOURS:
-            pos['exit_time'] = ft
-            pos['exit_price'] = fr['close']
-            pos['exit_reason'] = 'time_exit'
-            return pos
-    return None  # still open
-
-def compute_position_pnl(pos):
-    """Compute PnL in dollars for a closed position."""
-    pair = pos['pair']
-    pip = pip_size(pair)
-    if pos['direction'] == 'long':
-        gross_pips = (pos['exit_price'] - pos['e
+   'палистая дет породсься коминостьенно
