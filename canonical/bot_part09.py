db4414a8b5509aadd3d1673978d0e8d5eece340c
donkreat-eng <donkreat@yahoo.com>
2026-09-27 00:09:29 +0500

    Restore bot.py part 09 (corrected)

--- canonical/bot_part09.py
+++ canonical/bot_part09.py
@@ -1,15 +1,12 @@
-ibztime(df[time_col], errors='coerce').strftime('%0H:%1S')
-    return df
-
-def compute_position_pnl(pos, price, pip_size):
-    """Compute current PnL for an open position."""
-    if pos['direction'] == 'long':
-        pips = (pos['entry_price'] - price) / pip_size
-    else:
-        pips = (price - pos['entry_price']) / pip_size
-    pip_value_per_lot_usd = pip_value_per_lot_usd(pos['pairi'], pos['entry_price'])
-    pnl_usd = pips * pos['lot_size'] * pip_value_per_lot_usd
-    return {'pips': pips, 'pnl_usd': pnl_usd, 'is_open': True}
-
-def format_signal_message(sig, pair):
-    """Format a trading signal message."""
+_price(pair) / 2
+                pos['exit_reason'] = 'stop'
+                return pos
+            if fr['high'] >= pos['target']:
+                pos['exit_time'] = ft
+                pos['exit_price'] = pos['target'] - spread_price(pair) / 2
+                pos['exit_reason'] = 'target'
+                return pos
+        else:
+            if fr['high'] >= pos['stop']:
+                pos['exit_time'] = ft
+                pos['exit_price'] 
