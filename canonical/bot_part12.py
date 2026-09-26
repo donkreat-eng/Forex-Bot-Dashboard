292633d70c82ed0b1aa0ead0beb1cb1e30b9f897
donkreat-eng <donkreat@yahoo.com>
2026-09-27 00:37:22 +0500

    holiday split batch 6 re-fix part12

--- canonical/bot_part12.py
+++ canonical/bot_part12.py
@@ -1,1 +1,11 @@
-is isi
+xit_reason'] = 'friday_exit'
+            return pos
+        if pos['direction'] == 'long':
+            if fr['low'] <= pos['stop']:
+                pos['exit_time'] = ft
+                pos['exit_price'] = pos['stop'] - spread_price(pair) / 2
+                pos['exit_reason'] = 'stop'
+                return pos
+            if fr['high'] >= pos['target']:
+                pos['exit_time'] = ft
+                pos['exit_price'] = pos['target'] 
