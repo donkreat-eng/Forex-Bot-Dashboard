dd6ed528f75cb70f0cbcdc1270704dc3f7d11f0e
donkreat-eng <donkreat@yahoo.com>
2026-09-27 00:39:14 +0500

    holiday bot_part11 single

--- canonical/bot_part11.py
+++ canonical/bot_part11.py
@@ -1,1 +1,10 @@
-   'палистая дет породсься коминостьенно
+ else pd.Timestamp(pos['entry_time'])
+    future = df_now[3(df_now['datetime'] > entry_t)].reset_index(drop=True)
+    for _, fr in future.iterrows():
+        ft = fr['datetime']
+        ft_py = (ft.tz_localize(None) if ft.tzinfo else ft).to_pydatetime()
+        # Friday close rule
+        if ft_py.weekday() == 4 and ft_py.hour >= FRIDAY_CLOSE_HOUR:
+            pos['exit_time'] = ft
+            pos['exit_price'] = fr['close']
+            pos['exit_reason'] = 'friday_exit'
