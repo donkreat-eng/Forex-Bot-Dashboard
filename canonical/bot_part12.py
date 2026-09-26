2f424758ec8ae409d5b85ee542aca09289ddf59d
donkreat-eng <donkreat@yahoo.com>
2026-09-27 00:37:34 +0500

    holiday batch 6 part12 FIX

--- canonical/bot_part12.py
+++ canonical/bot_part12.py
@@ -1,11 +1,10 @@
-xit_reason'] = 'friday_exit'
-            return pos
-        if pos['direction'] == 'long':
-            if fr['low'] <= pos['stop']:
-                pos['exit_time'] = ft
-                pos['exit_price'] = pos['stop'] - spread_price(pair) / 2
-                pos['exit_reason'] = 'stop'
-                return pos
-            if fr['high'] >= pos['target']:
-                pos['exit_time'] = ft
-                pos['exit_price'] = pos['target'] 
+info else pd.Timestamp(pos['entry_time'])
+    future = df_now[(df_now['datetime'] > entry_t)].reset_index(drop=True)
+    for _, fr in future.iterrows():
+        ft = fr['datetime']
+        ft_py = (ft.tz_localize(None) if ft.tzinfo else ft).to_pydatetime()
+        # Friday close rule
+        if ft_py.weekday() == 4 and ft_py.hour >= FRIDAY_CLOSE_HOUR:
+            pos['exit_time'] = ft
+            pos['exit_price'] = fr['close']
+            po
